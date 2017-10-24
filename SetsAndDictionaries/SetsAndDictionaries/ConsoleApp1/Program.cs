1b92becb2fc1fafd87c19a5ecbb660903880894a
fineti <fineti@gmail.com>
2017-10-24 11:03:48 +0300

    sets

---
+++ SetsAndDictionaries/SetsAndDictionaries/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            SortedSet<string> parking = new SortedSet<string>();
+
+            while(input != "END")
+            {
+                string[] tokens = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                if (tokens[0] == "IN")
+                {
+                    parking.Add(tokens[1]);
+                }
+                else
+                    parking.Remove(tokens[1]);
+
+                input = Console.ReadLine();
+            }
+
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
---
+++ SetsAndDictionaries/SetsAndDictionaries/SoftUniParty/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> invites = new SortedSet<string>();
+            SortedSet<string> notCome = new SortedSet<string>();
+
+            string input = Console.ReadLine();
+
+            while(input != "PARTY")
+            {
+                invites.Add(input);
+                input = Console.ReadLine();
+            }
+            input = Console.ReadLine();
+            while (input != "END")
+            {
+                foreach (var person in invites)
+                {
+                    if(input != person)
+                    {
+                        notCome.Add(input);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(notCome.Count);
+            foreach (var person in notCome)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
